96c99b9b46201e81ebc717e4f5cf80a81f520983
man4j <man4j@ya.ru>
2025-01-04 14:32:01 +0300

    fixes

--- train/gemma/dpo.py
+++ train/gemma/dpo.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import logging
 from tuningtron import Tuner
 
@@ -7,4 +7,4 @@ logging.basicConfig(level=logging.INFO)
 os.environ["HF_TOKEN"] = "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"
 
 tuner = Tuner("./gemma_sft", enable_deepspeed=False)
-tuner.dpo("equiron-ai/translator_dpo", "adapter_gemma_dpo", rank=16, batch_size=1, gradient_steps=1, learning_rate=1e-5)
+tuner.dpo("equiron-ai/translator_dpo", "adapter_gemma_dpo", rank=16)
--- train/gemma/sft.py
+++ train/gemma/sft.py
@@ -8,4 +8,4 @@ logging.basicConfig(level=logging.INFO)
 os.environ["HF_TOKEN"] = "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"
 
 tuner = Tuner("google/gemma-2-9b-it", enable_deepspeed=False)
-tuner.sft("equiron-ai/translator_sft", "adapter_gemma_sft", rank=64, batch_size=1, gradient_steps=1, learning_rate=1e-4)
+tuner.sft("equiron-ai/translator_sft", "adapter_gemma_sft", rank=128, batch_size=1, gradient_steps=1, learning_rate=1e-4)
